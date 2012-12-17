9bceba4e7d1d27bdca1a2dd4ef1f8d8cf5195c7c
Tobias Sundstrand <tobias@freeb.ee>
2012-12-17 22:05:02 +0100

    Fixed build warnings
    Fixed some buildwarning for the redundant call to stringWithString.

--- instagram-ios-sdk/instagram-ios-sdk/IGRequest.m
+++ instagram-ios-sdk/instagram-ios-sdk/IGRequest.m
@@ -133,7 +133,7 @@ NSString* const InstagramErrorDomain = @"instagramErrorDomain";
                                data:[NSString stringWithFormat:
                                      @"Content-Disposition: form-data; filename=\"%@\"\r\n", key]];
                 [self utfAppendBody:body
-                               data:[NSString stringWithString:@"Content-Type: image/png\r\n\r\n"]];
+                               data:@"Content-Type: image/png\r\n\r\n"];
                 [body appendData:imageData];
             } else {
                 NSAssert([dataParam isKindOfClass:[NSData class]],
@@ -142,7 +142,7 @@ NSString* const InstagramErrorDomain = @"instagramErrorDomain";
                                data:[NSString stringWithFormat:
                                      @"Content-Disposition: form-data; filename=\"%@\"\r\n", key]];
                 [self utfAppendBody:body
-                               data:[NSString stringWithString:@"Content-Type: content/unknown\r\n\r\n"]];
+                               data:@"Content-Type: content/unknown\r\n\r\n"];
                 [body appendData:(NSData*)dataParam];
             }
             [self utfAppendBody:body data:endLine];
